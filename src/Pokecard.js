0d5769aaadbf02b27439a51c5b080a2ea32a9874
Norman Herrera <norman.herrera@ymail.com>
2022-08-15 18:51:27 -0700

    pokegame component, winning/losing logic

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import './Pokecard.css';
-// const POKE_API =
-// 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'
 
 let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3): number)
