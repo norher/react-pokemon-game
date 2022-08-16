7905240a334bd03ea1b5ac48976bca0dd504aa67
Norman Herrera <norman.herrera@ymail.com>
2022-08-15 18:06:13 -0700

    pokedex component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import Pokecard from './Pokecard';
+import Pokedex from './Pokedex';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Pokecard 
-      id={4}
-      name="Charmander"
-      type="fire"
-      experience={62}/>
+      <Pokedex />
     </div>
   );
 }
